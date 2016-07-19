1b238b2fe691396dc4b22627c2bafd29923692a8
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2016-07-20 07:26:51 +0900

    revise

--- app.js
+++ app.js
@@ -4,5 +4,5 @@ const http = require('http');
 
 http.createServer((req, res) => {
 	res.writeHead(200, {'content-type': 'text/html'});
-	res.end('<p>hello world 2016-07-19 16:27 JST</p>');
+	res.end('<p>hello world 2016-07-20 07:07 JST</p>');
 }).listen(process.env.PORT || 3000);
